30ee60c9e15f44bf193bda5289205dfbaf55eb14
Илья В. <289477456+kingston256gb@users.noreply.github.com>
2026-07-12 10:51:44 +0300

    🗃️ database: upgreded user table and added table for banned users

--- Backend/app/models/user.py
+++ Backend/app/models/user.py
@@ -1,5 +1,10 @@
+from datetime import datetime, timezone
+from time import timezone
+
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import ForeignKey
+
+from enum import Enum
 
 from app.models.base import Base, intpk
 
@@ -13,6 +18,7 @@ class UserTable(Base):
         unique=True
     )
     password_hash: Mapped[str] = mapped_column(nullable=False)
+    telegram_id: Mapped[int] = mapped_column(nullable=False, unique=True)
 
 
 class UserStatsTable(Base):
@@ -34,3 +40,39 @@ class UserStatsTable(Base):
     idle: Mapped[int]
     cooldown: Mapped[int]
     up: Mapped[int]
+
+
+
+class BanCode(str, Enum):
+    JL = "JL"
+    JS = "JS"
+    AS = "AS"
+    MB = "MB"
+    HD = "HD"
+
+    @property
+    def desc(self) -> str:
+        descs = {
+            BanCode.JL: "Your JWT or cookies were stolen or you sent them to another user",
+            BanCode.JS: "This JWT is owned by another user",
+            BanCode.AS: "API spam detected",
+            BanCode.MB: "Banned by Administrator",
+            BanCode.HD: "Hacker detected. SQL injection or malicious payload attempt"
+        }
+        return descs.get(self)
+
+
+
+class BannedUserTable(Base):
+    __tablename__ = 'banned_users'
+
+    id: Mapped[intpk]
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey('users.id', ondelete='CASCADE'),
+    )
+    ban_code: Mapped[BanCode] = mapped_column(nullable=False)
+    banned_at: Mapped[datetime] = mapped_column(
+        default=lambda: datetime.now(timezone.utc)
+    )
+    ban_duration: Mapped[int | None]
+    is_active: Mapped[bool] = mapped_column(default=True)
